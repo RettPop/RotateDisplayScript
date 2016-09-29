37e2e9ef098fdf5ec18f3632ec07eeeb9c08d523
Rett Pop <RettPop@users.noreply.github.com>
2016-09-29 16:00:39 +0300

    Update rotatedisplay.sh
    Some variables added

--- rotatedisplay.sh
+++ rotatedisplay.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-if [[ $(xrandr | grep DP3 | awk '{print $3}') == "primary" ]]; then
-	if [[ $(xrandr | grep DP3 | awk '{print $5}') == "left" ]]; then
-		xrandr --output DP3 --rotate normal
-	else
-		xrandr --output DP3 --rotate left
-	fi
+DISPLNAME="DP3"
+
+if [[ $1 != "" ]]; then
+        DISPLNAME=$1
+fi
+
+echo "Going to rotate display $DISPLNAME"
+
+if [[ $(xrandr | grep $DISPLNAME | awk '{print $3}') == "primary" ]]; then
+        POSITION="{print \$5}"
 else
-	if [[ $(xrandr | grep DP3 | awk '{print $4}') == "left" ]]; then
-		xrandr --output DP3 --rotate normal
-	else
-		xrandr --output DP3 --rotate left
-	fi
+        POSITION="{print \$4}"
+fi
+echo $POSITION
+
+echo "Executing: xrandr | grep $DISPLNAME | awk '$POSITION'"
+xrandr | grep $DISPLNAME | awk '$POSITION'
+
+if [[ $(xrandr | grep $DISPLNAME | awk "$POSITION") == "left" ]]; then
+        xrandr --output $DISPLNAME --rotate normal
+        echo "xrandr --output $DISPLNAME --rotate normal"
+else
+        xrandr --output $DISPLNAME --rotate left
+        echo "xrandr --output $DISPLNAME --rotate left"
 fi
