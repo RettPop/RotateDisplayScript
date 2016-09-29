54d8e6df8093503c8839956ca07a91904b8baf58
Rett Pop <RettPop@users.noreply.github.com>
2016-09-29 16:01:43 +0300

    Update rotatedisplay.sh
    Minus debug lines

--- rotatedisplay.sh
+++ rotatedisplay.sh
@@ -13,15 +13,15 @@ if [[ $(xrandr | grep $DISPLNAME | awk '{print $3}') == "primary" ]]; then
 else
         POSITION="{print \$4}"
 fi
-echo $POSITION
-
-echo "Executing: xrandr | grep $DISPLNAME | awk '$POSITION'"
-xrandr | grep $DISPLNAME | awk '$POSITION'
+#debug
+#echo $POSITION
+#echo "Executing: xrandr | grep $DISPLNAME | awk '$POSITION'"
+#xrandr | grep $DISPLNAME | awk '$POSITION'
 
 if [[ $(xrandr | grep $DISPLNAME | awk "$POSITION") == "left" ]]; then
         xrandr --output $DISPLNAME --rotate normal
-        echo "xrandr --output $DISPLNAME --rotate normal"
+#        echo "xrandr --output $DISPLNAME --rotate normal"
 else
         xrandr --output $DISPLNAME --rotate left
-        echo "xrandr --output $DISPLNAME --rotate left"
+#        echo "xrandr --output $DISPLNAME --rotate left"
 fi
